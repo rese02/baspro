828ce5803139b5d58442c0a8f7629800fcc77d5a
Rene Senoner <senonerrene@hotmail.com>
2024-01-03 16:30:36 +0100

    init commit

---
+++ 231217_newecostyle/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/min.css">
+    <title>Nachhaltige Textilprodukte aus Innsbruck</title>
+</head>
+<header>
+    <?php include 'navigation.php'; ?>
+    
+</header>
+<body>
+
+
+
+    <?php include 'footer.php'; ?>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
